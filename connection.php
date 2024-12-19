0ebd8918e2c25e53aa6726333368be469a6d735f
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-20 01:20:43 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ $username = "root";
 $password = "";
 $dbname = "mirchirider";
 
-$conn = mysqli_connect ($servername,$username,$password,$dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if($conn)
 {
