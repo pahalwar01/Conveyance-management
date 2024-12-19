983291540675fac04e6f3d099d8a9979fb83eee9
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-20 00:09:41 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,19 @@
 <?php
 
-$conn = mysqli_connect($hostname = 'localhost','root','','mirchirider');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mirchirider";
+
+$conn = mysqli_connect ($servername,$username,$password,$dbname);
+
+if($conn)
+{
+    echo "Connection OK";
+}
+else
+{
+    echo "Connection Failed";
+}
 
 ?>
