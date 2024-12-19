223ea9ed0e4c580438af5018593ac8f9d3d70b32
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-20 00:22:53 +0530

    Create works.js

---
+++ user_1/apps/works/js/works.js
@@ -0,0 +1,20 @@
+var add_item = document.getElementById("addwork");
+
+add_item.onclick = function()
+{
+    var bg = document.getElementById("jobs_bg");
+    bg.style.display = "block";
+    var user_email = sessionStorage.getItem("user");
+    var json_data = localStorage.getItem(user_email);
+    var obj_data = JSON.parse(json_data);
+    var rider = document.getElementById("r_name");
+    rider.value = atob(obj_data.username);
+    
+}
+
+var close = document.getElementById("close_btn");
+var add_job = document.getElementById("add_job");
+close.onclick = function()
+{
+    window.location.replace("works.php");
+}
