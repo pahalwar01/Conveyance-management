2b8b247c64274dac9b7cd603cf8bcd01184a4aa4
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-20 12:15:27 +0530

    Update conveyance-management.sql

--- conveyance-management.sql
+++ conveyance-management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2024 at 07:40 AM
+-- Generation Time: Dec 20, 2024 at 07:45 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,9 +30,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `rider` (
   `rider_id` int(3) NOT NULL,
   `rider_name` text NOT NULL,
-  `rider_phone` int(13) NOT NULL,
+  `rider_phone` text NOT NULL,
   `password` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rider`
+--
+
+INSERT INTO `rider` (`rider_id`, `rider_name`, `rider_phone`, `password`) VALUES
+(101, 'Dharampal', '9555424849', 'Dharampal@983'),
+(102, 'Karankumar', '9873333660', 'Karan@983'),
+(103, 'Lalitkumar', '9999618106', 'Lalit@983');
 
 -- --------------------------------------------------------
 
