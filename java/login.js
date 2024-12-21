0a62ddd01d0d3e417539f5286898c796f541a056
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-21 20:18:00 +0530

    Update login.js

--- java/login.js
+++ java/login.js
@@ -1,9 +1,16 @@
-var signuppage = document.getElementById('signup_link');
-var loginpage = document.getElementById('signup_btn');
-var signup = document.getElementById('signup');
-var login = document.getElementById('login');
+var login_link = document.getElementById("login_link");
+var signup_link = document.getElementById("signup_link");
+var signup_box = document.getElementById("signup");
+var login_box = document.getElementById("login");
+    
+login_link.onclick = function()
+{
+    signup_box.style.display = "none";
+    login_box.style.display = "block";
+}
 
-signuppage.click = function() {
-  login.style.display = 'none';
-  signup.style.display = 'block';   
-};
+signup_link.onclick = function()
+{
+    login_box.style.display = "none";
+    signup_box.style.display = "block";
+}
