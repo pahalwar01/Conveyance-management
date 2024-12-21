1c3093bd896dfb68423f7b2475af08e518e6a6bd
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-21 21:16:02 +0530

    Update signin.php

--- codes/signin.php
+++ codes/signin.php
@@ -2,29 +2,26 @@
 
 //Login code starts here
 
-if(isset($_POST['email']) && isset($_POST['password']))
-{
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM rider WHERE email='$email' AND password='$password'";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-    if($row)
-    {
-        echo "Login Success";
-        header("Location: ../user_1/user_1.php");
-    }
-    else
-    {
-        echo "Login Failed";
-    }
-}
-else
-{
-    echo "Invalid Request";
-}
+    $sql = "SELECT * FROM rider WHERE Email = '$email'";
+    $result = $conn->query($sql);
 
+    if ($result->num_rows > 0) {
+        $rider = $result->fetch_assoc();
+        if (password_verify($password, $rider['password'])) {
+            $_SESSION['RiderID'] = $rider['RiderID']; // Session set karein
+            $_SESSION['Name'] = $rider['Name'];
+            echo "Login successful! Welcome " . $rider['Name'];
+        } else {
+            echo "Invalid password!";
+        }
+    } else {
+        echo "No rider found with this email!";
+    }
+}
 // Login code ends here
 
 
