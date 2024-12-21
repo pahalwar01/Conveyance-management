5c9d24fae4d379422a1eb74751ce4e581c24c1d4
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-21 20:13:55 +0530

    Update login.js

--- java/login.js
+++ java/login.js
@@ -1,22 +1,9 @@
-function login()
-{
-    var user = document.getElementById("login_uname").value;
-    var pass = document.getElementById("login_pwd").value;
-    var admin = user =="admin" && pass =="admin@123"? "login sucess":"Failed";
-    var first = user=="dharampal" && pass=="dharampal@123"? "login sucess": "Failed";
-    var second = user=="lalit" && pass=="lalit@123"? "login sucess": "Failed";
-    var third = user=="karan" && pass=="karan@123"? "login sucess": "Failed";
-    if (admin=="login sucess") {
-        window.location.href='user_1/user_1.php';
-    } 
-    if (first=="login sucess") {
-        window.location.href='dharampalmain.html';
-    } 
-    if (second=="login sucess") {
-        window.location.href='lalitmain.html';
-    } 
-    if (third=="login sucess") {
-        window.location.href='karanmain.html';
-    }
-    return false;
-}
+var signuppage = document.getElementById('signup_link');
+var loginpage = document.getElementById('signup_btn');
+var signup = document.getElementById('signup');
+var login = document.getElementById('login');
+
+signuppage.click = function() {
+  login.style.display = 'none';
+  signup.style.display = 'block';   
+};
