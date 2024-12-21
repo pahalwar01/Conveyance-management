6c651630bc1db1bb2a873506c003563893f9fc97
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-21 16:58:39 +0530

    Update conveyance-management.sql

--- conveyance-management.sql
+++ conveyance-management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2024 at 07:45 AM
+-- Generation Time: Dec 21, 2024 at 12:27 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
