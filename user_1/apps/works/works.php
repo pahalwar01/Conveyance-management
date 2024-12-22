4b9fecd070688c547d4997619292d6e84f12b36d
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-22 13:57:39 +0530

    Update works.php

--- user_1/apps/works/works.php
+++ user_1/apps/works/works.php
@@ -63,28 +63,75 @@ if (!isset($_SESSION['rider_id'])) {
     <!--Navigation coding end-->
     
     <!--section coding start-->
-
+<section>
     <div class="col-12">
         <div class="marquee-bg">
             <marquee behavior="sliding" direction="left" scrollamount="5" class="marquee-text"><i>LIST VIEW OF YOUR TOTAL JOBS DONE IN THIS MONTH, HOPE YOU DONE THIS MONTH A GOOD WORK, SEE YOU IN NEXT MONTH, GOOD LUCK !</i></marquee>
         </div>
-        
-    
-        <div id="dharampal" style="display: none;">
-            <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR7woZy9d8HSFhhdTebakHbzs27ZeYZmEpvr85jmyx_d2xb0ddWFGgn8G3-DwZxKHbeRa8nCvCgiGOp/pubhtml?gid=1952733198&amp;single=true&amp;widget=true&amp;headers=false" style="width: 100%; height: 720px;"></iframe>
-        </div>
 
-        <div id="karan" style="display: none;">
-            <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR7woZy9d8HSFhhdTebakHbzs27ZeYZmEpvr85jmyx_d2xb0ddWFGgn8G3-DwZxKHbeRa8nCvCgiGOp/pubhtml?gid=380888060&amp;single=true&amp;widget=true&amp;headers=false" style="width: 100%; height: 720px;"></iframe>
-        </div>
-        <div id="lalit" style="display: none;">
-            <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR7woZy9d8HSFhhdTebakHbzs27ZeYZmEpvr85jmyx_d2xb0ddWFGgn8G3-DwZxKHbeRa8nCvCgiGOp/pubhtml?gid=549043537&amp;single=true&amp;widget=true&amp;headers=false" style="width: 100%; height: 720px;"></iframe>
-        </div>
+        <div>
+            
+            <?php
 
-        <div id="mirchitasks" style="display: none;">
+                $display =  "SELECT * FROM rides WHERE rider_name = '$user'";
+                $rides = mysqli_query($conn, $display);
+
+                $total = mysqli_num_rows($rides);
+
+                // echo $total;
+
+                if($total != 0)
+                {
+                    ?>
+
+                    <h2 align="center"><mark>Displaying All Records</mark></h2>
+                    <center>
+                    <table border="1" cellspacing="5" width="100%">
+                        <tr>
+                        <th width="5%">ID</th>
+                        <th width="10%">Rider Name</th>
+                        <th width="10%">Sender Name</th>
+                        <th width="10%">Date</th>
+                        <th width="15%">Work Type</th>
+                        <th width="20%">From</th>
+                        <th width="15%">To</th>
+                        <th width="10%">Total KM</th>
+                        <th width="15%">Operations</th>
+                        </tr>
+                    
+                    <?php
+                    while ($result = mysqli_fetch_assoc($rides)) 
+                    {
+                        echo "<tr>
+                                <td>".$result['ride_id']."</td>
+                                <td>".$result['rider_name']."</td>
+                                <td>".$result['sender_name']."</td>
+                                <td>".$result['w_date']."</td>
+                                <td>".$result['work_type']."</td>
+                                <td>".$result['start_from']."</td>
+                                <td>".$result['end_to']."</td>
+                                <td>".$result['km']."</td>
+
+                                <td><a href='update_design.php?update_id=$result[id]'>Update</a></td>
+                            </tr>
+                            ";
+                    }
+                        }
+                        else
+                        {
+                            echo "No records found";
+                        }
+
+                    ?>
+                    </table>
+                    </center>
+
+
+        </div>
         
-            
-            
+        <div id="#" style="display: none;">
+        
+                
         </div>
         
     </div>
@@ -98,6 +145,7 @@ if (!isset($_SESSION['rider_id'])) {
             <h1>Add New Job</h1><br>
             <form id="add_work" action="../action.php" method="POST">
             <!-- <form id="add_work" name="submit-to-google-sheet" method="POST"> -->
+                <input type="hidden" name="riderid" value="<?php echo $_SESSION['rider_id']; ?>">
                 <p id="add">Your Name</p>
                 <input type="text" name="rname" id="ridername" required="required" value="<?php echo $_SESSION['rider_name']; ?>"></input>
                 <p id="add">Sender Name</p>
@@ -120,9 +168,8 @@ if (!isset($_SESSION['rider_id'])) {
         </div>
         </center>
     </div>
-
+</section>
     <!--section coding end-->
-
 
 
     
