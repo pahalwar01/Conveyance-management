f3b368cfab511d587b2189d441bba086d3dfe7f0
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-29 23:03:08 +0530

    Update user_signup.php

--- user_signup.php
+++ user_signup.php
@@ -13,6 +13,7 @@ include 'codes/signup.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="img/logo.png"> 
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
