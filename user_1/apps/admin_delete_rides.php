0ce754a37642fa65c91c20660ebac8effed58c04
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2025-01-04 15:43:14 +0530

    Update admin_delete_rides.php

--- user_1/apps/admin_delete_rides.php
+++ user_1/apps/admin_delete_rides.php
@@ -61,7 +61,7 @@ if (!isset($_SESSION['admin_id'])) {
                 <li class="col-3"><a href="../admin_dashboard.php"><i class="fas fa-home"></i> Home</a></li>
                 <li class="col-3" id="jobview"><a href="../admin_jobview.php"><i class="fa-solid fa-print"></i> Print</a></li>
                 <li class="col-3" id="jobview"><a href="admin_delete_rides.php"><i class="fa-solid fa-trash"></i> Delete</a></li>
-                <li class="col-3" id="logout"><a href="#"><p id="logout_text"><i class="fa-solid fa-right-from-bracket"></i> <?php echo '<a href="logout.php">Logout</a>'; ?> </p></a></li>
+                <li class="col-3" id="logout"><a href="#"><p id="logout_text"><i class="fa-solid fa-right-from-bracket"></i> <?php echo '<a href="../logout.php">Logout</a>'; ?> </p></a></li>
             </ul>       
         </nav>
     </div>
