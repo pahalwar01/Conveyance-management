5adffffe27c17dd396ac78f436701176ed54b78d
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2025-01-04 15:39:27 +0530

    Update admin_dashboard.php

--- user_1/admin_dashboard.php
+++ user_1/admin_dashboard.php
@@ -60,9 +60,10 @@ if (!isset($_SESSION['admin_id'])) {
     <div class="row">
         <nav class="col-12">
             <ul>
-                <li class="col-4"><a href="admin_dashboard.php"><i class="fas fa-home"></i> Home</a></li>
-                <li class="col-4" id="jobview"><a href="admin_jobview.php"><i class="fas fa-list"></i> Jobs View</a></li>
-                <li class="col-4" id="logout"><a href="#"><p id="logout_text"><i class="fa-solid fa-right-from-bracket"></i> <?php echo '<a href="logout.php">Logout</a>'; ?> </p></a></li>
+                <li class="col-3"><a href="admin_dashboard.php"><i class="fas fa-home"></i> Home</a></li>
+                <li class="col-3" id="jobview"><a href="admin_jobview.php"><i class="fa-solid fa-print"></i> Print</a></li>
+                <li class="col-3" id="jobview"><a href="apps/admin_delete_rides.php"><i class="fa-solid fa-trash"></i> Delete</a></li>
+                <li class="col-3" id="logout"><a href="#"><p id="logout_text"><i class="fa-solid fa-right-from-bracket"></i> <?php echo '<a href="logout.php">Logout</a>'; ?> </p></a></li>
             </ul>        
         </nav>
     </div>
