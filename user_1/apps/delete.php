71d754cb55a668b2969d839ee717564c20f9a675
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2025-01-05 16:59:38 +0530

    Create delete.php

---
+++ user_1/apps/delete.php
@@ -0,0 +1,82 @@
+<?php
+include '../../connection/connection.php'
+?>
+
+<?php
+session_start();
+
+$user = $_SESSION['admin_name'];
+
+// यदि यूजर लॉगिन नहीं है, तो उसे लॉगिन पेज पर रिडायरेक्ट करें
+if (!isset($_SESSION['admin_id'])) {
+    header("Location: ../logout.php");
+    exit();
+}
+
+// डैशबोर्ड कंटेंट
+// echo "<h1>स्वागत है, " . $_SESSION['rider_name'] . "!</h1>";
+// echo "<p>यह आपका डैशबोर्ड है।</p>";
+// echo '<a href="logout.php">लॉगआउट</a>';
+?>
+
+<html>
+<head>
+    <title>For Delete Rides List By Month</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" type="image/x-icon" href="../../img/logo.png"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+</head>
+
+<div id="displayfornonprint">
+    <!--header coding start-->
+<div class="row">
+        <header class="col-12">
+            <div>
+                <img src="../../img/logo.png" alt="logo" id="logo">
+            </div>
+            
+            <h2><center style="height: 50px; text-align: center; color: red;"><span id="profile_name"><?php echo $_SESSION['admin_name'] . "!</h4>"; ?></span></center></h2>
+            
+        </header>    
+    </div>
+    <!--header coding end-->
+
+    <!--Navigation coding start-->
+    <div class="row">
+        <nav class="col-12">
+            <ul>
+                <li class="col-12"><a href="admin_delete_rides.php"><i class="fas fa-arrow-left"></i> Back</a></li>
+            </ul>
+            
+        </nav>
+    </div>
+    <!--Navigation coding end-->
+</div>
+<div>
+<?php
+
+$ride_id=$_GET['ride_id'];
+$query = "DELETE from rides WHERE ride_id = '$ride_id'";
+
+$delete=mysqli_query($conn,$query);
+
+if ($delete){
+    echo "<font color='red'>Record Deleted From Database";
+    // header('Location:delete_rides_list.php');
+}
+else{
+    echo "<font color='red'>Failed to Delete Record From Database";
+}
+
+?>
+
+</div>
+</html>
