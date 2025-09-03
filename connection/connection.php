29dc5c157f40e22b44d21cf7060d3a70ea602e3e
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2025-09-03 20:53:56 +0530

    Update connection.php

--- connection/connection.php
+++ connection/connection.php
@@ -1,15 +1,15 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "conveyance-management";
+$username = "myconvey_conveyance";
+$password = "yDJLMnXugcteF8WWv2NU";
+$dbname = "myconvey_conveyance";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if($conn)
 {
-    echo "Connection OK";
+    // echo "Connection OK";
 }
 else
 {
